744cf6153b9ca1a6db04869c33a539a1d71b3f26
KristopherToll <tol05007@byui.edu>
2017-02-09 20:45:39 -0700

    Commiting!

---
+++ Toll/Cleaning_the_Data.R
@@ -0,0 +1,4 @@
+#Importing the DATA!
+library(readxl)
+bob <- read_excel("C:/Users/Kristopher/odrive/Google Drive/Water Transfer Project/Raw_Data/Water_Transfer_Data_Feb_10.xls", na = "null")
+View(bob)
