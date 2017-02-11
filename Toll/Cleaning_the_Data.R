01e6cdc2f5880f63ad97b56e196a2c5b2535df4d
KristopherToll <tol05007@byui.edu>
2017-02-11 12:00:59 -0700

    here they are

--- Toll/Cleaning_the_Data.R
+++ Toll/Cleaning_the_Data.R
@@ -2,3 +2,12 @@
 library(readxl)
 bob <- read_excel("C:/Users/Kristopher/odrive/Google Drive/Water Transfer Project/Raw_Data/Water_Transfer_Data_Feb_10.xls", na = "null")
 View(bob)
+# Removing "na" from bob
+gerald <- subset(bob, TotalPrice != "NA" 
+                 & MinimumAnnualAcreFeet != "NA" 
+                 & AverageAnnualAcreFeet != "NA"
+                 & MaximumAnnualAcreFeet != "NA"
+                 & bob$CommittedMinimumAcreFeet != "NA"
+                 & bob$CommitedAverageAcreFeet != "NA"
+                 & bob$CommitedMaximumAcreFeet != "NA"
+                 & bob$`InflationAdjustedTotal Price`)
