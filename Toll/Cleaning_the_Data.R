8a989e71475138a85b952bbd10d40b432f116b79
KristopherToll <tol05007@byui.edu>
2017-02-11 18:55:37 -0700

    just moving along

--- Toll/Cleaning_the_Data.R
+++ Toll/Cleaning_the_Data.R
@@ -1,7 +1,10 @@
-#Importing the DATA!
+
+# challeng only use nemo names!
+# Importing the DATA!
 library(readxl)
 nemo <- read_excel("C:/Users/Kristopher/odrive/Google Drive/Water Transfer Project/Raw_Data/Water_Transfer_Data_Feb_10.xls", na = "null")
 View(nemo)
+
 # Removing "na" from nemo
 dory <- subset(nemo, TotalPrice != "NA" 
                  & MinimumAnnualAcreFeet != "NA" 
@@ -13,13 +16,22 @@ dory <- subset(nemo, TotalPrice != "NA"
                  & nemo$InflationAdjustedTotalPrice != "NA"
                  & nemo$InflationAdjustedPricePerAnnualAcreFoot != "NA"
                  & nemo$InflationAdjustedPricePerCommittedAcreFoot != "NA")
+
 # Removing Lease Observations
 # To remove Sales use a one instead
 bruce <- subset(dory, Lease == "0")
 View(bruce)
+
 # Remove the recycled vectors and the Lease Duration Vector and others
-merlin <- bruce[,-c(bruce$Unknown, 
-                    bruce$Recycled,
-                    bruce$Combination,
-                    )]
+merlin <- bruce
+merlin$Unknown <- NULL
+merlin$Recycled <- NULL
+merlin$LeaseDuration <- NULL
 View(merlin)
+
+# Now to fix time!
+# Some observations are recored in two months such as July/Aug 
+# abreviations are not always used
+table(merlin$WaterStrategistIssue)
+
+
